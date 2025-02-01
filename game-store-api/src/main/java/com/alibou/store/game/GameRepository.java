931b6b2551c80d74ccd71c11f06d9a326cbb5937
Sifeddine <sifeddinebaouz@gmail.com>
2025-02-01 16:59:24 +0100

    Complete the deleteGame method
    - Delete the game from the wishlists.
    - Delete the game comments.
    - Add the dev profile.

--- game-store-api/src/main/java/com/alibou/store/game/GameRepository.java
+++ game-store-api/src/main/java/com/alibou/store/game/GameRepository.java
@@ -3,7 +3,10 @@ package com.alibou.store.game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 public interface GameRepository extends JpaRepository<Game, String>, JpaSpecificationExecutor<Game> {
 
     boolean existsByTitle(String title);
+
 }
